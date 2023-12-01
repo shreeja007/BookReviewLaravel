3cdbdb3523298778954c44548efb6e8914e7b0d7
Shreeja Manandhar <manandharja@gmail.com>
2023-12-01 20:47:26 +0545

    db conect

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,6 +3,9 @@
 namespace Database\Seeders;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+
+use App\Models\Book;
+use App\Models\Review;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
@@ -12,11 +15,11 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        // \App\Models\User::factory(10)->create();
+        Book::factory(33)->create()->each(function($book){
+            $numReview = random_int(5,30);
+            
+            Review::factory()->count($numReview)->good()->for()
 
-        // \App\Models\User::factory()->create([
-        //     'name' => 'Test User',
-        //     'email' => 'test@example.com',
-        // ]);
+        });
     }
 }
