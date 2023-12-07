dd1d2441b970bd8f28bb5ed9e4c236c79341764a
Shreeja Manandhar <manandharja@gmail.com>
2023-12-07 21:11:07 +0545

    view

---
+++ resources/views/books/index.blade.php
@@ -0,0 +1,63 @@
+@extends('layouts.app')
+
+@section('content')
+ <h1 class="mb-10 text-2x1">asd</h1>
+
+
+<form action="{{ route('books.index')}}" method="GET" class="flex mb-4 items-center space-x-2">
+<input type="text" name="title" placeholder="Enter Title" value="{{ request('title') }}" class="input h-10"/>
+<button type="submit" class="btn h-10">Search</button>
+<a href="{{ route('books.index')}}" class="btn h-10"> Clear</a>
+</form>
+
+<div class="filter-container mb-4 flex">
+
+    @php
+     $filters = [
+        'latest' => 'latest',
+        'popular_last_month' => 'Popular Last Month',
+        'popular_last_6months' => 'Popular Last 6 Months',
+        'highest_rated_last_month' => 'Highest Rated Last Month',
+        'highest_rated_last_6month' => 'Highest Rated Last 6 Months'
+     ]   
+    @endphp
+
+    @foreach ($filters as $key => $label)
+        <a href="{{ route('books.index', ['filter' => $key]) }}" 
+        class="{{ request('filter') === $key ? 'filter-item-active' : 'filter-item'}}">
+            {{ $label }}
+        </a>
+    @endforeach
+
+</div>
+ <ul>
+    @forelse ($books as $book)
+    <li class="mb-4">
+        <div class="book-item">
+          <div
+            class="flex flex-wrap items-center justify-between">
+            <div class="w-full flex-grow sm:w-auto">
+              <a href="{{ route('books.show', $book) }}" class="book-title">{{ $book->title }}</a>
+              <span class="book-author">by {{ $book->author }}</span>
+            </div>
+            <div>
+              <div class="book-rating">
+                {{ number_format($book->reviews_avg_rating, 1) }}
+              </div>
+              <div class="book-review-count">
+                out of {{ $book->reviews_count}} {{ Str::plural('review', $book->reviews_count)}}
+              </div>
+            </div>
+          </div>
+        </div>
+      </li>
+    @empty
+    <li class="mb-4">
+        <div class="empty-book-item">
+          <p class="empty-text">No books found</p>
+          <a href="{{ route('books.index')}}" class="reset-link">Reset criteria</a>
+        </div>
+      </li>
+    @endforelse
+ </ul>
+@endsection
