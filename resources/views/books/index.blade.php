05f829562b01b773174e3331e0348bd5fd1c48bf
Shreeja Manandhar <manandharja@gmail.com>
2023-12-10 22:18:04 +0545

    13 complte blade complte

--- resources/views/books/index.blade.php
+++ resources/views/books/index.blade.php
@@ -14,17 +14,19 @@
 
     @php
      $filters = [
-        'latest' => 'latest',
+        '' => 'latest',
         'popular_last_month' => 'Popular Last Month',
         'popular_last_6months' => 'Popular Last 6 Months',
         'highest_rated_last_month' => 'Highest Rated Last Month',
         'highest_rated_last_6month' => 'Highest Rated Last 6 Months'
-     ]   
+     ]
     @endphp
 
     @foreach ($filters as $key => $label)
-        <a href="{{ route('books.index', ['filter' => $key]) }}" 
-        class="{{ request('filter') === $key ? 'filter-item-active' : 'filter-item'}}">
+
+        <a href="{{ route('books.index', [...request()->query(), 'filter' => $key]) }}"
+
+        class="{{ request('filter') === $key || (request('filter') === null && $key === '') ? 'filter-item-active' : 'filter-item'}}">
             {{ $label }}
         </a>
     @endforeach
